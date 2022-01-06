e00814772a620211bf967ef8cae1f0770b051eaf
kaydee0502 <kshitijdhama513@gmail.com>
2022-01-06 21:18:38 +0530

    basic ground div

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,29 @@ import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import "./styles/output.css";
+import Ground from "./Ground";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="bg-gray-900 p-20 h-screen flex justify-center items-start flex-col">
-      <h1 className="text-5xl text-white">XitiZ</h1>
-      <p className="text-gray-400 mt-5 text-lg">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
-        consequuntur odio aut nobis ab quis? Reiciendis doloremque ut quo fugiat
-        eveniet tempora, atque alias earum ullam inventore itaque sapiente iste?
-      </p>
-      <button class="p-4 bg-green-600 rounded-lg font-bold text-white mt-5 hover:bg-gray-600">
-        Hello Friends 🚀
-      </button>
-    </div>
+    <>
+      <div className="bg-gray-900 p-20 h-screen flex items-start flex-col">
+        <h1 className="text-5xl text-white">XitiZ</h1>
+        <p className="text-gray-400 mt-5 text-lg">
+          Hi Devs, my name is Kshitij Dhama, a Software Developer who loves
+          problem solving and spending hours on obvious bugs xD
+        </p>
+        <div class="w-full flex justify-center my-5">
+          <div class="w-11/12">
+            <Ground />
+          </div>
+        </div>
+        <button class="p-4 bg-green-600 rounded-lg font-bold text-white mt-5 hover:bg-gray-600">
+          Hello Friends 🚀
+        </button>
+      </div>
+    </>
   );
 }
 
---
+++ src/Ground.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Ground = () => {
+  return <h1 class="p-5 bg-white">Hello</h1>;
+};
+
+export default Ground;

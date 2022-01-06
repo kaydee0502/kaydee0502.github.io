a31be1b7963d4e3bea247a324f7a9b21d7bca869
kaydee0502 <kshitijdhama513@gmail.com>
2022-01-06 22:57:14 +0530

    upgrade tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import "./styles/output.css";
+import "./index.css";
 import Ground from "./Ground";
 
 function App() {
@@ -15,12 +15,12 @@ function App() {
           Hi Devs, my name is Kshitij Dhama, a Software Developer who loves
           problem solving and spending hours on obvious bugs xD
         </p>
-        <div class="w-full flex justify-center my-5">
-          <div class="w-11/12">
+        <div className="w-full flex justify-center my-5">
+          <div className="w-11/12">
             <Ground />
           </div>
         </div>
-        <button class="p-4 bg-green-600 rounded-lg font-bold text-white mt-5 hover:bg-gray-600">
+        <button className="p-3 bg-green-600 rounded-lg text-white mt-5 hover:bg-gray-600">
           Hello Friends 🚀
         </button>
       </div>
--- src/Ground.jsx
+++ src/Ground.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Ground = () => {
-  return <h1 class="p-5 bg-white">Hello</h1>;
+  return (
+    <div className="p-5 bg-gray-700 rounded-lg">
+      Hello
+      <div className="h-0.5 w-full bg-zinc-600"></div>
+    </div>
+  );
 };
 
 export default Ground;

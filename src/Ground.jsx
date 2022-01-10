8fe5aaf01396afcefaa5b9ce84ecb65a81235461
kaydee0502 <kshitijdhama513@gmail.com>
2022-01-10 22:30:54 +0530

    added list boxs

--- src/Ground.jsx
+++ src/Ground.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Verb from "./Verb";
+import Endpoint from "./Endpoint";
 
 const Ground = () => {
   return (
     <div className="p-5 bg-gray-700 rounded-lg">
       The know about
       <div className="h-0.5 w-full bg-gray-600"></div>
+      <div className="w-3/4">
+        <Verb></Verb>
+        <Endpoint></Endpoint>
+      </div>
     </div>
   );
 };
